b44e0e3b361a10a1b65f4b9067545f469170a076
Laurent FIDAHOUSSEN <10000.maniacs@gmail.com>
2022-12-12 22:48:50 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div>
       <h2>Let's get started</h2>
-      <p>This is also visible</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
