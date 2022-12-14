d40f56b57f7db49dc24a2498a500918f99f29871
Laurent FIDAHOUSSEN <10000.maniacs@gmail.com>
2022-12-14 11:26:54 +0100

    38 - update css and components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,11 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css' ;
 
 function ExpenseItem(props) {
- 
-  const month = props.date.toLocaleString('us-EN', {month: 'long'}) ;
-  const year = props.date.getFullYear() ;
-  const day = props.date.toLocaleString('us-EN', {day: '2-digit'}) ;
 
   return (
     <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
