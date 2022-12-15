c05985ad67ce23045c17325ef666cd5556a93964
Laurent FIDAHOUSSEN <10000.maniacs@gmail.com>
2022-12-15 10:54:06 +0100

    53 - update components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -32,6 +33,7 @@ const App = () => {
   return (
     <div>
       <h2>Let's get started</h2>
+      <NewExpense />
       <Expenses items={expenses}/>
     </div>
   );
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css' ;
@@ -5,17 +7,19 @@ import './ExpenseItem.css' ;
 
 const ExpenseItem = (props) => {
 
-let title = props.title ;
+const [title, setTitle] = useState(props.title) ;
+console.log('ExpenseItem evaluated by React') ;
 
 const clickHandler = () => {
-    title = 'Updated!' ;
+  setTitle('Updated!') ;
+  console.log('Updated!') ;
  } ;
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
