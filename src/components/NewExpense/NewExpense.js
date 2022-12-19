2ce492949d62eaa3dd5d3745f3e21f2dd9e22964
Laurent FIDAHOUSSEN <10000.maniacs@gmail.com>
2022-12-19 10:44:39 +0100

    58 - last updates

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -4,13 +4,14 @@ import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
 
-const saveExpenseDataHandler = (enteredExpenseData) => {
-    const expenseData = {
-        ...enteredExpenseData,
-        id: Math.random().toString()
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        console.log('new expense (1):', expenseData) ;
+        props.onAddExpense(expenseData) ;
     };
-    props.onAddExpense(expenseData) ;
-};
 
     return <div className='new-expense'>
         <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
